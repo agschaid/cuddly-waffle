da8a68e0619d645c19a492d7151309cee589ba3e
Axel Gschaider <axel.gschaider@posteo.de>
2022-09-27 12:14:40 +0200

    add simple map test

--- src/test/java/at/agschaid/NullInMapsTest.java
+++ src/test/java/at/agschaid/NullInMapsTest.java
@@ -3,6 +3,17 @@ package at.agschaid;
 import io.micronaut.runtime.EmbeddedApplication;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Map;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.junit.jupiter.api.Assertions;
 
 import jakarta.inject.Inject;
@@ -13,9 +24,21 @@ class NullInMapsTest {
     @Inject
     EmbeddedApplication<?> application;
 
+    @Inject
+    ObjectMapper objectMapper;
+
     @Test
     void testItWorks() {
         Assertions.assertTrue(application.isRunning());
+    }
+
+    @Test
+    void testNullInMap() throws JsonMappingException, JsonProcessingException {
+      final String mapWithNullValue = "{\"foo\":\"bar\", \"testNull\": null}";
+      Map<?,?> untypedMap = objectMapper.readValue(mapWithNullValue, Map.class);
+
+      assertTrue(untypedMap.containsKey("testNull"));
+      assertNull(untypedMap.get("testNull"));
     }
 
 }
