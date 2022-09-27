97885e81be3341f243100d23ac022f262fb17cee
Axel Gschaider <axel.gschaider@posteo.de>
2022-09-27 11:08:04 +0200

    create minimal micronaut project using launch
    https://micronaut.io/launch/

---
+++ src/test/java/at/agschaid/NullInMapsTest.java
@@ -0,0 +1,21 @@
+package at.agschaid;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class NullInMapsTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
